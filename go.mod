3da11c454c9eba86bfc941d83c4a81eb1c9e6e8c
anurag kanwar <aigkaraig@gmail.com>
2024-11-26 17:56:58 +0530

    asjddf;aojna

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module danm
 
 go 1.23.1
 
-require github.com/oxffaa/gopher-parse-sitemap v0.0.0-20191021113419-005d2eb1def4 // indirect
+require github.com/oxffaa/gopher-parse-sitemap v0.0.0-20191021113419-005d2eb1def4
--- main.go
+++ main.go
@@ -1,23 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 
 	sitemap "github.com/oxffaa/gopher-parse-sitemap"
 )
 
+type Person struct {
+	Name string `json:"name"`
+	Age  string `json:"age"`
+}
+
 func main() {
 	sitemapPath := os.Args[1]
+	filePath := os.Args[2]
+	fmt.Println(sitemapPath)
 	result := make([]string, 0)
-	err := sitemap.ParseIndexFromFile(sitemapPath, func(e sitemap.IndexEntry) error {
+	err := sitemap.ParseIndexFromSite(sitemapPath, func(e sitemap.IndexEntry) error {
 		result = append(result, e.GetLocation())
 		return nil
 	})
 
-	if err == nil {
-		fmt.Println(err)
+	if err != nil {
+		fmt.Printf("Error: %+v\n", err)
 	} else {
 		fmt.Println(result)
 	}
+
+	jsonFile, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer jsonFile.Close()
+
+	person1 := &Person{}
+	byteValye, err := io.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	json.Unmarshal(byteValye, person1)
+
+	fmt.Println(person1.Name, person1.Age)
 }
